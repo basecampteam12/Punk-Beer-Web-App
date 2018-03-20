5194b7d15ca06a799009d4bae63c87b294b44d2a
Medard <med>
2018-03-19 21:32:15 -0700

    initial set up

--- src/index.js
+++ src/index.js
@@ -8,32 +8,30 @@ import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Router>
-        <div className="container">
-          <nav class="navbar navbar-default">
-              <div class="container-fluid">
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                  <a class="navbar-brand" href="#">Beans Love Beers</a>
-                </div>
-
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li ><NavLink exact activeClassName="activeNav" to="/">Home</NavLink></li>
-                        <li><NavLink activeClassName="activeNav" to="/favourites">Favourites</NavLink></li>
-                    </ul>
-                </div>
-              </div>
-            </nav>
-            <Route exact path="/" component={Home} />
-            <Route path="/favourites" component={Favourites} />
-        </div>
-    </Router>, document.getElementById('root')
-);
+	<Router>
+		<div className="container">
+	      <nav className="navbar navbar-default">
+	          <div className="container-fluid">
+	            <div className="navbar-header">
+	              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	                <span className="sr-only">Toggle navigation</span>
+	                <span className="icon-bar"></span>
+	                <span className="icon-bar"></span>
+	                <span className="icon-bar"></span>
+	              </button>
+	              <a className="navbar-brand" href="#">Beans Love Beers</a>
+	            </div>
+	            <div className="collapse navbar-collapse">
+	                <ul className="nav navbar-nav navbar-right">
+	                    <li><NavLink exact activeclassName="activeNav" to="/">Home</NavLink></li>
+	                    <li><NavLink activeclassName="activeNav" to="/favourites">Favourites</NavLink></li>
+	                </ul>
+	            </div>
+	          </div>
+	        </nav>
+	        <Route exact path="/" component={Home} />
+	        <Route path="/favourites" component={Favourites} />
+	    </div>
+    </Router>, document.getElementById('root'));
 
 registerServiceWorker();
