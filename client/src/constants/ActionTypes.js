99106058555b769eff073b660e45fc04db9058d3
basecampteam12 <basecampteam12@gmail.com>
2018-03-24 00:08:02 -0700

    Folder structure initial set up

---
+++ client/src/constants/ActionTypes.js
@@ -0,0 +1,5 @@
+export const CHANGE_ROUTE = 'CHANGE_ROUTE';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGOUT = 'LOGOUT';
+export const TOGGLE_LIKE = 'TOGGLE_LIKE';
+export const WINDOW_RESIZE = 'WINDOW_RESIZE';
---
+++ client/src/constants/ApiConstants.js
@@ -0,0 +1,7 @@
+const API_HOSTNAME = '//api.punkapi.com/v2'; 
+
+export const ALL_BEERS_URL = `${API_HOSTNAME}/beers`;
+export const SINGLE_BEEER_URL = `${ALL_BEERS_URL}/:id`;
+export const RANDOM_BEEER_URL = `${ALL_BEERS_URL}/random`;
+
+
